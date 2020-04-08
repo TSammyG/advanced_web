5cc628ff272b793c0bb55cb7b0112aa70d7dbd17
GSickley <greswellsickley@gmail.com>
2020-04-08 22:29:17 +0100

    Added profile.php

---
+++ profile.php
@@ -0,0 +1,15 @@
+<?php
+/* This include calls the code from header.php, letting us concentrate on the code unique to the page */
+include("includes/header.php");
+/*session_destroy();*/ // This code can be used to log the user out whenever they refresh their page
+?>
+
+<div class="main_column column">
+   This is a profile page
+</div>
+
+
+</div> <!-- This is opened in header.php -->
+</body>
+
+</html>
