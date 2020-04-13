49b9ca6a40e1a2d4a197dcc4b7b4359fc04e172f
GSickley <greswellsickley@gmail.com>
2020-04-13 17:00:22 +0100

    Have infinite scrolling working for first 10 posts.

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -1,187 +1,206 @@
 <?php
-
-class Post
-{
+class Post {
     private $user_obj;
     private $con;
 
-    public function __construct($con, $user)
-    {
+    public function __construct($con, $user){
         $this->con = $con;
         $this->user_obj = new User($con, $user);
     }
 
-    public function submitPost($body, $user_to)
-    {
-        $body = strip_tags($body); // Removes HTML tags
-        $body = mysqli_real_escape_string($this->con, $body); // Escapes any special characters with an apostrothat could be mistaken for a single quote, for example
+    public function submitPost($body, $user_to) {
+        $body = strip_tags($body); //Removes HTML tags
+        $body = mysqli_real_escape_string($this->con, $body);// Escapes any special characters with an apostrophe that could be mistaken for a single quote, for example
         $check_empty = preg_replace('/\s+/', '', $body); //Deletes all spaces
 
-        // For recognising new lines
-        $body = str_replace('\r\n', '\n', $body); // Looks for a carried return followed by a new line
-        $body = nl2br($body); // nl = new line, 2 = to, br = line break
+        if($check_empty != "") {
 
 
-        if ($check_empty != "") {
             //Current date and time
             $date_added = date("Y-m-d H:i:s");
             //Get username
             $added_by = $this->user_obj->getUsername();
 
-            //IF user is on own profile, user_to is 'none'
-            if ($user_to == $added_by) {
+            //If user is on own profile, user_to is 'none'
+            if($user_to == $added_by) {
                 $user_to = "none";
             }
 
-            //Insert post
-            mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', 
-                '$date_added', 'no', 'no', '0')");
-            mysqli_insert_id($this->con);
+            //insert post
+            $query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+            $returned_id = mysqli_insert_id($this->con);
 
             //Insert notification
 
             //Update post count for user
-            $num_post = $this->user_obj->getNumPosts();
-            //Increases post count by 1
-            $num_post++;
-            mysqli_query($this->con, "UPDATE users SET num_post='$num_post' 
-                WHERE username='$added_by'");
+            $num_posts = $this->user_obj->getNumPosts();
+            $num_posts++;
+            $update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
+
         }
     }
 
-    private function pluraliseMessage($interval, $unit_of_time)
-    {
-        $pluralisedMessage = $unit_of_time;
-
-        if ($interval != 1) {
-            $pluralisedMessage .= 's';
-        }
-
-        return $pluralisedMessage;
-    }
-
-    private function createSingleTimeMessage($interval, $unit_of_time)
-    {
-        return "{$interval} {$this->pluraliseMessage($interval,$unit_of_time)} ago";
-    }
-
-    private function getTimeMessage($interval)
-    {
-        $time_message = "";
-        if ($interval->y > 0) {
-            $time_message = $this->createSingleTimeMessage($interval->y, "year");
-        } else if ($interval->m > 0) {
-            $days = $this->createSingleTimeMessage($interval->d, "day");
-            $time_message = $this->createSingleTimeMessage($interval->m, "month") . $days;
-        } else if ($interval->d > 0) {
-            $time_message = $interval->d == 1 ? "Yesterday" : $this->createSingleTimeMessage($interval->d, "day");
-        } else if ($interval->h > 0) {
-            $time_message = $this->createSingleTimeMessage($interval->h, "hour");
-        } else if ($interval->i > 0) {
-            $time_message = $this->createSingleTimeMessage($interval->i, "minute");
-        } else if ($interval->s > 0) {
-            $time_message = $this->createSingleTimeMessage($interval->s, "second");
-        } else {
-            $time_message = "Just now";
-        }
-        return $time_message;
-    }
-
-    public function loadPostsFriends($data, $limit)
-    {
+    public function loadPostsFriends($data, $limit) {
 
         $page = $data['page'];
         $userLoggedIn = $this->user_obj->getUsername();
 
-        if ($page == 1)
+        if($page == 1)
             $start = 0;
         else
             $start = ($page - 1) * $limit;
 
-        $post_str = ""; // String to return
+
+        $str = ""; //String to return
         $data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
 
-        if (mysqli_num_rows($data_query) > 0) {
+        if(mysqli_num_rows($data_query) > 0) {
 
-            $num_iterations = 0; // No. of results checked, though not necessarily posted
+
+            $num_iterations = 0; //Number of results checked (not necasserily posted)
             $count = 1;
 
-            while ($row = mysqli_fetch_array($data_query)) {
+            while($row = mysqli_fetch_array($data_query)) {
                 $id = $row['id'];
                 $body = $row['body'];
                 $added_by = $row['added_by'];
-                $user_to = $row['user_to'];
                 $date_time = $row['date_added'];
 
                 //Prepare user_to string so it can be included even if not posted to a user
-
-                if ($user_to == "none") {
+                if($row['user_to'] == "none") {
                     $user_to = "";
-                } else {
-                    $user_to_obj = new User($this->con, $user_to);
+                }
+                else {
+                    $user_to_obj = new User($con, $row['user_to']);
                     $user_to_name = $user_to_obj->getFirstAndLastName();
-                    $user_to = "to <a href='{$user_to}'>$user_to_name</a>";
+                    $user_to = "to <a href='" . $row['user_to'] ."'>" . $user_to_name . "</a>";
                 }
 
                 //Check if user who posted, has their account closed
                 $added_by_obj = new User($this->con, $added_by);
-                if ($added_by_obj->isClosed()) {
+                if($added_by_obj->isClosed()) {
                     continue;
                 }
 
-                if ($num_iterations++ < $start)
+
+
+                if($num_iterations++ < $start)
                     continue;
 
 
                 //Once 10 posts have been loaded, break
-                if ($count > $limit) {
+                if($count > $limit) {
                     break;
-                } else {
+                }
+                else {
                     $count++;
                 }
 
-
-                $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic 
-                                                                FROM users WHERE username='$added_by'");
+                $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
                 $user_row = mysqli_fetch_array($user_details_query);
-
                 $first_name = $user_row['first_name'];
                 $last_name = $user_row['last_name'];
                 $profile_pic = $user_row['profile_pic'];
+
 
                 //Timeframe
                 $date_time_now = date("Y-m-d H:i:s");
                 $start_date = new DateTime($date_time); //Time of post
                 $end_date = new DateTime($date_time_now); //Current time
                 $interval = $start_date->diff($end_date); //Difference between dates
-                $time_message = $this->getTimeMessage($interval);
+                if($interval->y >= 1) {
+                    if($interval == 1)
+                        $time_message = $interval->y . " year ago"; //1 year ago
+                    else
+                        $time_message = $interval->y . " years ago"; //1+ year ago
+                }
+                else if ($interval-> m >= 1) {
+                    if($interval->d == 0) {
+                        $days = " ago";
+                    }
+                    else if($interval->d == 1) {
+                        $days = $interval->d . " day ago";
+                    }
+                    else {
+                        $days = $interval->d . " days ago";
+                    }
 
-                $post_str .= "<div class='status_post'>
-                                <div class='post_profile_pic'>
-                                    <img src='$profile_pic' width='50'>
-                                </div>
 
-                                <div class='posted_by' style='color:#ACACAC;'>
-                                    <a href='$added_by'> 
-                                        $first_name $last_name 
-                                    </a> 
-                                    $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
-                                </div>
+                    if($interval->m == 1) {
+                        $time_message = $interval->m . " month". $days;
+                    }
+                    else {
+                        $time_message = $interval->m . " months". $days;
+                    }
 
-                                <div id='post_body'>
-                                    $body
-                                    <br>
-                                </div>
-                            </div>
-                            </hr>";
-            }
+                }
+                else if($interval->d >= 1) {
+                    if($interval->d == 1) {
+                        $time_message = "Yesterday";
+                    }
+                    else {
+                        $time_message = $interval->d . " days ago";
+                    }
+                }
+                else if($interval->h >= 1) {
+                    if($interval->h == 1) {
+                        $time_message = $interval->h . " hour ago";
+                    }
+                    else {
+                        $time_message = $interval->h . " hours ago";
+                    }
+                }
+                else if($interval->i >= 1) {
+                    if($interval->i == 1) {
+                        $time_message = $interval->i . " minute ago";
+                    }
+                    else {
+                        $time_message = $interval->i . " minutes ago";
+                    }
+                }
+                else {
+                    if($interval->s < 30) {
+                        $time_message = "Just now";
+                    }
+                    else {
+                        $time_message = $interval->s . " seconds ago";
+                    }
+                }
 
-            if ($count > $limit)
-                $post_str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
+                $str .= "<div class='status_post'>
+								<div class='post_profile_pic'>
+									<img src='$profile_pic' width='50'>
+								</div>
+
+								<div class='posted_by' style='color:#ACACAC;'>
+									<a href='$added_by'> $first_name $last_name </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+								</div>
+								<div id='post_body'>
+									$body
+									<br>
+								</div>
+
+							</div>
+							<hr>";
+
+
+            } //End while loop
+
+            if($count > $limit)
+                $str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
 							<input type='hidden' class='noMorePosts' value='false'>";
             else
-                $post_str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align: centre;'> No more posts to show! </p>";
+                $str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align: centre;'> No more posts to show! </p>";
         }
-        echo $post_str;
+
+        echo $str;
+
+
     }
+
+
+
+
 }
+
+?>
--- index.php
+++ index.php
@@ -11,58 +11,64 @@ if (isset($_POST['post'])) {
     header("Location: index.php"); // If a post has been made, stops it from being posted twice if page is refreshed
 }
 
+
 ?>
 <div class="user_details column">
     <!-- By separating them with a space we can have 2 classes -->
-    <a href="<?php echo $userLoggedIn; ?>"><img src="<?php echo $user['profile_pic']; ?>"/></a>
+    <a href="<?php echo $userLoggedIn; ?>">  <img src="<?php echo $user['profile_pic']; ?>"> </a>
+
     <div class="user_details_left_right">
         <a href="<?php echo $userLoggedIn; ?>">
             <?php
             echo $user['first_name'] . " " . $user['last_name'];
+
             ?>
-            <br>
         </a>
-        <?php
-        echo "Posts: " . $user['num_post'] . "<br>";
-        echo "Likes: " . $user['num_like'];
+        <br>
+        <?php echo "Posts: " . $user['num_posts']. "<br>";
+        echo "Likes: " . $user['num_likes'];
+
         ?>
     </div>
+
 </div>
+
 <div class="main_column column">
     <form class="post_form" action="index.php" method="POST">
-        <textarea name="post_text" id="post_text" placeholder="What's on your mind?"></textarea>
+        <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
         <input type="submit" name="post" id="post_button" value="Post">
         <hr>
-    </form>
 
+    </form>
 
     <div class="posts_area"></div>
     <img id="loading" src="assets/images/icons/loading.gif">
+
 
 </div>
 
 <script>
     var userLoggedIn = '<?php echo $userLoggedIn; ?>';
 
-    $(document).ready(function () {
+    $(document).ready(function() {
+
         $('#loading').show();
 
-        // Original ajax request for loading first posts
+        //Original ajax request for loading first posts
         $.ajax({
             url: "includes/handlers/ajax_load_posts.php",
             type: "POST",
             data: "page=1&userLoggedIn=" + userLoggedIn,
-            cache: false,
+            cache:false,
 
-            success: function (data) {
+            success: function(data) {
                 $('#loading').hide();
                 $('.posts_area').html(data);
             }
         });
 
-
-        $(window).scroll(function () {
-            var height = $('.posts_area').height(); // Div containing posts
+        $(window).scroll(function() {
+            var height = $('.posts_area').height(); //Div containing posts
             var scroll_top = $(this).scrollTop();
             var page = $('.posts_area').find('.nextPage').val();
             var noMorePosts = $('.posts_area').find('.noMorePosts').val();
@@ -73,29 +79,32 @@ if (isset($_POST['post'])) {
                 var ajaxReq = $.ajax({
                     url: "includes/handlers/ajax_load_posts.php",
                     type: "POST",
-                    data: "page=" + page + "&userLoggedIn" + userLoggedIn,
-                    cache: false,
+                    data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                    cache:false,
 
-                    success: function (response) {
+                    success: function(response) {
                         $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage
-                        $('.posts_area').find('.noMorePosts').remove(); //Removes current .nomoreposts
+                        $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage
 
                         $('#loading').hide();
                         $('.posts_area').append(response);
-
                     }
                 });
 
-            } //end if
+            } //End if
 
             return false;
-        }); //End scroll function
+
+        }); //End (window).scroll(function())
+
 
     });
+
 </script>
+
+
 
 
 </div> <!-- This is opened in header.php -->
 </body>
-
 </html>
